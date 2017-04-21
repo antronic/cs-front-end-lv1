8e010204f4cb845a3ca339c5c7c144f95810121a
Jirachai Chansivanon <antronic.inc@gmail.com>
2017-04-21 21:46:40 +0700

    New document updated

---
+++ 03-Bootstrap/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'html': {
+    'backgroundColor': '#000'
+  },
+  'body': {
+    'backgroundColor': '#000'
+  }
+});
